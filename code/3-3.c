4d0e9d90826e6f0df569ccf72e753d535c61beb7
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-04-03 10:16:38 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -4,10 +4,8 @@ int main(){
     int val = 7;
     int other = 77;
 
-    int *p = &val;
-    *p = 10;
-    p = &other;
+    const int *const p = &val;
     
-    printf("%d\n", p);
+    printf("%d\n", *p);
     return 0;
 }
